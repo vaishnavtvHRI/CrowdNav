3fa5f03e2c44be399fc7cc3b0a25e6eeb0c5e711
VietAnh Le <vale@dt871512lx.am.mds.honda.com>
2023-08-24 17:50:04 -0400

    something wrong with sfm, no human move

--- crowd_sim/envs/policy/sfm.py
+++ crowd_sim/envs/policy/sfm.py
@@ -6,10 +6,12 @@ import pysocialforce as psf
 class SFM(Policy):
     def __init__(self):
         super().__init__()
+        self.name = 'SFM'
         self.trainable = False
         self.kinematics = 'holonomic'
         self.multiagent_training = True
         self.sim = None
+
 
     def configure(self, config):
         self.config_file = config
@@ -26,9 +28,9 @@ class SFM(Policy):
         """
         self_state = state.self_state
 
-        if self.sim is not None and self.sim.getNumAgents() != len(state.human_states) + 1:
-            del self.sim
-            self.sim = None
+        # if self.sim is not None and self.sim.getNumAgents() != len(state.human_states) + 1:
+        #     del self.sim
+        #     self.sim = None
         if self.sim is None: # simulator has not been initialized
             initial_state = np.array(
                 [
@@ -36,7 +38,7 @@ class SFM(Policy):
                 ]
             )
             for human_state in state.human_states:
-                initial_state = np.vstack([initial_state, np.array([human_state.px, human_state.py, human_state.vx, human_state.vy, human_state.gx, human_state.gy])])
+                initial_state = np.vstack([initial_state, np.array([human_state.px, human_state.py, human_state.vx, human_state.vy, human_state.px, human_state.py])])
 
             # initiate the simulator, hardcode the config file for now
             self.sim = psf.Simulator(
@@ -44,15 +46,15 @@ class SFM(Policy):
                 config_file="/home/vale/github/mpc-nav/PySocialForce/examples/example.toml",
             )
         else:
-            state = np.array(
+            initial_state = np.array(
                 [
                     [self_state.px, self_state.py, self_state.vx, self_state.vy, self_state.gx, self_state.gy],
                 ]
             )
             for human_state in state.human_states:
-                initial_state = np.vstack([initial_state, np.array([human_state.px, human_state.py, human_state.vx, human_state.vy, human_state.gx, human_state.gy])])
+                initial_state = np.vstack([initial_state, np.array([human_state.px, human_state.py, human_state.vx, human_state.vy, human_state.px, human_state.py])])
 
-            self.sim.peds.update(state, None)
+            self.sim.peds.update(initial_state, None)
 
         self.sim.step()
         action = ActionXY(*self.sim.peds.vel()[0,:])
